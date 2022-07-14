7bab9dfd5eabfb56789a05bdfa30f4a2bc616575
nakamura3 <nakamu@ines.co.jp>
2022-07-14 04:41:26 +0000

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "56jtpdumocltphbvakl4qe8cg8",     // CognitoClientID
-  "api_base_url": "https://dt99ov8gil.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "68apoie8c7061uuai0rek34n9b",     // CognitoClientID
+  "api_base_url": "https://wku2hduzha.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "inestodoapp-lambda-refarch-webapp-stack.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1aec8koiw76om.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d1eq4kxcuiqvge.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
